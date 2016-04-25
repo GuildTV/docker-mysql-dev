373e5c80f348519dbf7a9a4bc6abc482bc59aca9
Julian Waller <git@julusian.co.uk>
2016-04-26 00:03:53 +0100

    Add a second db for testing (prefixed with Test_)

--- setup.sh
+++ setup.sh
@@ -8,14 +8,20 @@ EXISTS=`mysql -p$MYSQL_ROOT_PASSWORD -e 'SHOW DATABASES LIKE "$MYSQL_USERNAME";'
 if [ -e "/example.sql" ] && [ -z "$EXISTS" ]; then    
   # Create a database with that name
   mysqladmin -u root -p$MYSQL_ROOT_PASSWORD create $MYSQL_USERNAME
+  mysqladmin -u root -p$MYSQL_ROOT_PASSWORD create "Test_$MYSQL_USERNAME"
 
   # Create a new user with same name as new db, with password 'pass'
   `mysql -u root -p$MYSQL_ROOT_PASSWORD -e "\
       GRANT ALL PRIVILEGES ON $MYSQL_USERNAME.* TO '$MYSQL_USERNAME'@'%' \
       IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; \
       FLUSH PRIVILEGES; "`
+  `mysql -u root -p$MYSQL_ROOT_PASSWORD -e "\
+      GRANT ALL PRIVILEGES ON "Test_$MYSQL_USERNAME.*" TO '$MYSQL_USERNAME'@'%' \
+      IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; \
+      FLUSH PRIVILEGES; "`
 
   # Import the SQL into new database
   `mysql -u root -p$MYSQL_ROOT_PASSWORD "$MYSQL_USERNAME" < /example.sql`
+  `mysql -u root -p$MYSQL_ROOT_PASSWORD "Test_$MYSQL_USERNAME" < /example.sql`
 
 fi
